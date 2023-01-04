0742ba9a8163f29547804f9f1901ad5f753731b6
Michael Hope <hopemichael720@gmail.com>
2023-01-04 12:36:52 +0000

    Added Iterators

--- string_vectors_arrays/vectors/main.cpp
+++ string_vectors_arrays/vectors/main.cpp
@@ -24,11 +24,21 @@ int main(){
 
     vector<double> dvec {2.23, 2.32, 5.34};
 
+    // Iterators
     string s("some string"); 
     if(s.begin() != s.end()){ // make sure s is not empty
         auto it = s.begin(); // it denotes the first character in s
         *it = toupper(*it); // make that character uppercase 
     }
+
+    // double vec elements
+    vector<int> vec {1, 2, 8, 5, 4};
+    for (auto i = vec.begin(); i != vec.end(); ++i){
+        *i = *i * 2;
+    }
+
+    for (auto i :  vec)
+        cout << "vec: " << i <<  endl;
         
 
 }
